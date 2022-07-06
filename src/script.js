53df4202dc9bcb896d76670b7c554cd83f36ad5a
Abishethvarman V <abishethvarman@gmail.com>
2022-07-07 00:20:11 +0530

    final

--- src/script.js
+++ src/script.js
@@ -2,12 +2,7 @@
 let weather = {
   apiKey: "aba6ff9d6de967d5eac6fd79114693cc",
   fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" +city +"&units=metric&appid=" +this.apiKey)
       .then((response) => {
         if (!response.ok) {
           alert("No weather found.");
@@ -16,6 +11,7 @@ let weather = {
         return response.json();
       })
       .then((data) => this.displayWeather(data));
+      // .then((displayWeather) => this.message1(displayWeather));
   },
   displayWeather: function (data) {
     const { name, visibility,timezone } = data;
@@ -24,16 +20,14 @@ let weather = {
     const { temp, humidity, feels_like, temp_min, temp_max, pressure } = data.main;
     const { speed } = data.wind;
     const { country,sunrise,sunset } = data.sys;
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".country").innerText = "," + country;
-    // document.querySelector(".lan").innerText = "Lattitude " + lat;
 
+    document.querySelector(".city").innerText = "Weather in " + name+", "+country;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".main").innerText = "Mainly " + main;
     document.querySelector(".sunrise").innerText = "Sunrise " + moment.utc(sunrise,'X').add(timezone,'seconds').format('HH:mm a');
     document.querySelector(".sunset").innerText = "Sunset at " + moment.utc(sunset,'X').add(timezone,'seconds').format('HH:mm a');
     document.querySelector(".description").innerText = description;
-    document.querySelector(".visibility").innerText = visibility;
+    // document.querySelector(".visibility").innerText = visibility;
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".feels_like").innerText ="Feels Like: " + feels_like + "°C";
     document.querySelector(".temp_min").innerText = "Minimum Temperature: " + temp_min + "°C";
@@ -42,34 +36,13 @@ let weather = {
     document.querySelector(".pressure").innerText ="Pressure: " + pressure + "Hg";
     document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
-    
-    var ab = (temp>=10) ? "hot":"cool";
-    document.querySelector(".ab") ="a: " + ab + "Hg";
-
     document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
     document.querySelector(".temp").innerText = temp + "°C";
-  //   const a = ()=>{
-  //   if(this.temp>30){
-  //     console.log("suc")
-  //   }
-  // else{
-  //   console.log("wrong")
-  // }}
-  //   console.log(a);
 
-  //   document.querySelector(".a").innerText ="a: " + a + "Hg";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
-  // message: function(data) {
-
-  //   const { temp, humidity, feels_like, temp_min, temp_max, pressure } = data.main;
-  //   let a = (temp >30) ? "hot":"cool";
-    
-  //   document.querySelector(".a") ="a: " + a + "Hg";
-  // },
-
 };
 
 /* Fetching Data from OpenCageData Geocoder */
@@ -79,16 +52,7 @@ let geocode = {
 
     var api_url = "https://api.opencagedata.com/geocode/v1/json";
 
-    var request_url =
-      api_url +
-      "?" +
-      "key=" +
-      apikey +
-      "&q=" +
-      encodeURIComponent(latitude + "," + longitude) +
-      "&pretty=1" +
-      "&no_annotations=1";
-
+    var request_url =api_url +"?" +"key=" +apikey +"&q=" +encodeURIComponent(latitude + "," + longitude) +"&pretty=1" +"&no_annotations=1";
     var request = new XMLHttpRequest();
     request.open("GET", request_url, true);
 
@@ -150,3 +114,61 @@ document
   });
 
 geocode.getLocation();
+
+function msg1() {
+  let atemp = document.getElementById("temp").innerHTML;
+
+  if(atemp>30){
+    document.querySelector(".atemp").innerText =  "Veyil koluthuthu. Konjam melliya cotton sattaiya pottu ponga. Nalla thanni kudinga. 3L minimum sollipottan";
+  }
+  else{
+    document.querySelector(".atemp").innerText =  "Nallarukenna ithama?  ";
+  };
+  
+}
+
+function msg2() {
+  let aMain = document.getElementById("main").innerHTML;
+  // document.querySelector(".atemp").innerText = atemp + "indaikku malai ";
+  // let btemp;
+  switch(aMain) {
+    case 'Thunderstorm':
+      document.querySelector(".aMain").innerText =  "Indaikku idikkum kavanam";
+      break;
+    case 'Drizzle' :
+      document.querySelector(".aMain").innerText =  "Thoothala irukkum kavanam. Thadumalakkum";
+      break;
+    case 'Rain':
+      document.querySelector(".aMain").innerText =   "Mazhai peyyapoguthu. Kudaiyoda ponga. Rain coat um kondu ponga";
+      break;
+    case 'Snow':
+      document.querySelector(".aMain").innerText = "Pani peiyuthu sweater oda ponga";
+      break;
+     case 'Clear':
+      document.querySelector(".aMain").innerText = "Vaanam summa palichendu irukku";
+      break;
+    case 'Clouds':
+      document.querySelector(".aMain").innerText = "Vaanam mekamootathodu kaanapadum";
+      break;
+
+    default:
+      document.querySelector(".aMain").innerText = "intha naal iniya naal aaka amaiyattum";
+  };
+  
+}
+
+function msg3() {
+  let aMain2 = document.getElementById("main").innerHTML;
+  // document.querySelector(".atemp").innerText = atemp + "indaikku malai ";
+  // let btemp;
+  if(aMain2=="Clear"){
+    document.querySelector(".aMain2").innerText = "Ippa nallam velikkala seyatpaadukalukku";
+  }
+  else if(aMain2=="Clear Sky"){
+    document.querySelector(".aMain2").innerText = "Ippa nallam velikkala seyatpaadukalukku";
+  }
+  else{
+    document.querySelector(".aMain2").innerText =  "Kastam Bro";
+  };
+  
+}
